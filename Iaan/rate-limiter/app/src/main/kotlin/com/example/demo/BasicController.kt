117dac7ae0a5655e4fc16e2f1e1b098de8c7d44a
geuno <rlfrmsdh1@gmail.com>
2024-07-01 23:00:26 +0900

    iaan first commit

---
+++ Iaan/rate-limiter/app/src/main/kotlin/com/example/demo/BasicController.kt
@@ -0,0 +1,13 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/test")
+class BasicController {
+
+    @GetMapping
+    fun test() = "Hello World"
+}
